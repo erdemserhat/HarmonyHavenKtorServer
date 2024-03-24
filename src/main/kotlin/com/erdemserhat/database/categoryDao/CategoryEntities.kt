0dcbcd12714156f66bedb6d53ffa0cee60ca9b12
Serhat <me.serhaterdem@gmail.com>
2024-03-24 15:58:35 +0300

    --experimental production ver.1 prep

--- src/main/kotlin/com/erdemserhat/database/categoryDao/CategoryEntities.kt
+++ src/main/kotlin/com/erdemserhat/database/categoryDao/CategoryEntities.kt
@@ -6,7 +6,7 @@ import org.ktorm.entity.Entity
 import org.ktorm.schema.Table
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
-object DBCategoryTable : Table<DBCategoryEntity>("Categories"){
+object DBCategoryTable : Table<DBCategoryEntity>("categories"){
 
     val id= int("id").primaryKey().bindTo { it.id }
     val name = varchar("name").bindTo { it.name }
